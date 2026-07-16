a443d1182096444183e0eeda637cbd94581af05c
papieros <robertrugala@gmail.com>
2026-07-16 23:05:18 +0200

    create header only library cco.h

---
+++ cco.h
@@ -0,0 +1,326 @@
+#ifndef CCO_H
+#define CCO_H
+#include <memory.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#define CCO_SCHED_INIT_PROC_COUNT 10
+
+typedef enum cco_CState {
+    cco_READY,
+    cco_BLOCKED,
+    cco_DONE,
+} cco_CState;
+
+typedef struct cco_Coroutine cco_Coroutine;
+
+typedef void* cco_Ctx_p;
+
+typedef void (*cco_poll_fn)(cco_Coroutine* self, cco_Ctx_p ctx);
+
+typedef struct cco_Coroutine {
+    void* state;
+    cco_CState c_state;
+    cco_poll_fn poll;
+} cco_Coroutine;
+
+#define cco_make_coroutine(STATE, POLL) \
+    (cco_Coroutine)                     \
+    {                                   \
+        .state = STATE,                 \
+        .c_state = cco_READY,           \
+        .poll = POLL                    \
+    }
+
+typedef struct cco_Message {
+    char data[128];
+    int from_id;
+    int to_id;
+    struct cco_Message* prev;
+    struct cco_Message* next;
+} cco_Message;
+
+typedef enum cco_ProcFlags {
+    cco_MESSAGE_PENDING = 1,
+    cco_AWAITING_MESSAGE = 1 << 1,
+} cco_ProcFlags;
+
+typedef struct cco_Process {
+    cco_Coroutine co;
+    int flags;
+    cco_Message* msg;
+    char* recv_buf;
+    struct cco_Process *next, *prev;
+    int id;
+} cco_Process;
+
+typedef struct MessageQueue {
+    cco_Message* head;
+    cco_Message* tail;
+} cco_MessageQueue;
+
+typedef struct cco_ProcQueue {
+    cco_Process* head;
+    cco_Process* tail;
+} cco_ProcQueue;
+
+typedef struct cco_Sched {
+    cco_MessageQueue mq;
+    cco_Process** procs;
+    int proc_count, proc_cap;
+    cco_ProcQueue pqueue;
+} cco_Sched;
+
+
+typedef void* cco_Co_handle;
+
+cco_Sched cco_Sched_new();
+cco_Co_handle cco_Sched_add_coroutine(cco_Sched* sched, cco_Coroutine co);
+void cco_Sched_run(cco_Sched* sched);
+void cco_Sched_free(cco_Sched sched);
+
+void cco_send(cco_Coroutine* self, cco_Ctx_p ctx, cco_Co_handle co, char msg[128]);
+void cco_recv(cco_Coroutine* self, cco_Ctx_p ctx, char out[128]);
+
+void cco_yield(cco_Coroutine* co, cco_Ctx_p ctx);
+void cco_return(cco_Coroutine* co, cco_Ctx_p ctx);
+
+cco_Co_handle cco_spawn(cco_Ctx_p ctx, cco_Coroutine spawn);
+
+#ifdef CCO_IMPLEMENTATION
+
+typedef struct Context {
+    int c_id;
+    jmp_buf* restart;
+    cco_MessageQueue* msg_queue;
+    cco_Process** procs;
+    cco_Sched* sched;
+} cco_Context;
+
+static void cco_append_msg(cco_MessageQueue* queue, int from, int to, cco_Message* msg)
+{
+    msg->from_id = from;
+    msg->to_id = to;
+    if (!queue->head) {
+        queue->head = msg;
+        queue->tail = msg;
+        queue->head->next = NULL;
+        queue->head->prev = NULL;
+    } else {
+        queue->tail->next = msg;
+        msg->prev = queue->tail;
+        queue->tail = msg;
+        msg->next = NULL;
+    }
+}
+static void cco_recv_impl(cco_Coroutine* self, cco_Process* proc, char out[128])
+{
+    proc->recv_buf = out;
+    if ((proc->flags & cco_MESSAGE_PENDING) == cco_MESSAGE_PENDING || proc->msg != NULL) {
+        memcpy(proc->recv_buf,
+            proc->msg->data,
+            128);
+        self->c_state = cco_READY;
+        free(proc->msg);
+        proc->msg = NULL;
+        proc->recv_buf = NULL;
+    } else {
+        proc->flags |= cco_AWAITING_MESSAGE;
+        self->c_state = cco_BLOCKED;
+    }
+}
+
+static void cco_append_proc(cco_ProcQueue* queue, cco_Process* proc)
+{
+    if (!queue->head) {
+        queue->head = proc;
+        queue->tail = proc;
+        queue->head->next = NULL;
+        queue->head->prev = NULL;
+    } else {
+        queue->tail->next = proc;
+        proc->prev = queue->tail;
+        queue->tail = proc;
+        proc->next = NULL;
+    }
+}
+static cco_Process* cco_pop_proc(cco_ProcQueue* queue)
+{
+    if (!queue->head) {
+        return NULL;
+    }
+    cco_Process* pop = queue->head;
+    queue->head = queue->head->next;
+    return pop;
+}
+static cco_Message* cco_pop_msg(cco_MessageQueue* queue)
+{
+    if (!queue->head) {
+        return NULL;
+    }
+    cco_Message* pop = queue->head;
+    queue->head = queue->head->next;
+    return pop;
+}
+
+static void cco_deliver_messages(cco_MessageQueue* mq, cco_Process** procs, int procs_sz)
+{
+    cco_Message* delay_stack[128] = { };
+    int dp = -1;
+    while (mq->head) {
+        cco_Message* msg = cco_pop_msg(mq);
+        cco_Process* to = procs[msg->to_id - 1];
+        cco_Process* from = procs[msg->from_id - 1];
+        if (to->msg) {
+            delay_stack[++dp] = msg;
+            continue;
+        } else {
+            to->msg = msg;
+        }
+        to->co.c_state = cco_READY;
+        from->co.c_state = cco_READY;
+        if (to->recv_buf) {
+            cco_recv_impl(&to->co, to, to->recv_buf);
+        }
+    }
+    while (dp >= 0) {
+        cco_Message* delayed = delay_stack[--dp];
+        cco_append_msg(mq, delayed->from_id, delayed->to_id, delayed);
+    }
+}
+static void cco_Process_free(cco_Process* proc)
+{
+    if (proc->msg)
+        free(proc->msg);
+    if (proc->co.state)
+        free(proc->co.state);
+}
+
+cco_Sched cco_Sched_new()
+{
+    return (cco_Sched) {
+        .mq = (cco_MessageQueue) { },
+        .procs = (cco_Process**)calloc(CCO_SCHED_INIT_PROC_COUNT, sizeof(cco_Process*)),
+        .proc_count = 0,
+        .proc_cap = CCO_SCHED_INIT_PROC_COUNT,
+        .pqueue = (cco_ProcQueue) { },
+    };
+}
+cco_Co_handle cco_Sched_add_coroutine(cco_Sched* sched, cco_Coroutine co)
+{
+    if (sched->proc_count + 1 > sched->proc_cap) {
+        int new_cap = sched->proc_cap * 2;
+        int old_cap = sched->proc_cap;
+        sched->proc_cap = new_cap;
+        cco_Process** tmp = (cco_Process**)calloc(new_cap, sizeof(cco_Process*));
+        memcpy(
+            tmp,
+            sched->procs,
+            old_cap * sizeof(cco_Process*));
+        free(sched->procs);
+        sched->procs = tmp;
+    }
+    cco_Process* proc = (cco_Process*)malloc(sizeof(cco_Process));
+    *proc = (cco_Process) { };
+    proc->co = co;
+    sched->procs[sched->proc_count++] = proc;
+    proc->id = sched->proc_count;
+    cco_append_proc(&sched->pqueue, proc);
+    return (void*)(long)proc->id;
+}
+
+void cco_Sched_free(cco_Sched sched)
+{
+    for (int i = 0; i < sched.proc_count; i++) {
+        cco_Process* proc = sched.procs[i];
+        cco_Process_free(proc);
+    }
+    free(sched.procs);
+    cco_Message* msg = cco_pop_msg(&sched.mq);
+    while (msg) {
+        free(msg);
+        msg = cco_pop_msg(&sched.mq);
+    }
+}
+void cco_Sched_run(cco_Sched* sched)
+{
+    jmp_buf jmp_buffer = { };
+    while (1) {
+        int ret = setjmp(jmp_buffer);
+        if (ret != 0) {
+            cco_append_proc(&sched->pqueue, sched->procs[ret - 1]);
+        }
+        cco_deliver_messages(&sched->mq, sched->procs, sched->proc_count);
+        cco_Process* next = { };
+        next = cco_pop_proc(&sched->pqueue);
+        if (!next) {
+            break;
+        }
+        if (next->co.c_state == cco_BLOCKED) {
+            if ((next->flags & cco_MESSAGE_PENDING) != cco_MESSAGE_PENDING) {
+                cco_append_proc(&sched->pqueue, next);
+                continue;
+            }
+        }
+        if (next->co.c_state == cco_DONE) {
+            int _this = next->id - 1;
+            int _last = sched->proc_count - 1;
+            sched->procs[_this] = sched->procs[_last];
+            sched->procs[_last] = NULL;
+            sched->proc_count--;
+            cco_Process_free(next);
+            continue;
+        }
+        cco_Context ctx = {
+            .c_id = next->id,
+            .restart = &jmp_buffer,
+            .msg_queue = &sched->mq,
+            .procs = sched->procs,
+            .sched = sched,
+        };
+        next->co.poll(
+            &next->co,
+            &ctx);
+    }
+}
+void cco_recv(cco_Coroutine* self, cco_Ctx_p ctx, char out[128])
+{
+    cco_Context* context = (cco_Context*)ctx;
+    cco_Process* proc = context->procs[context->c_id - 1];
+    cco_recv_impl(self, proc, out);
+    longjmp(*context->restart, context->c_id);
+}
+void cco_yield(cco_Coroutine* co, cco_Ctx_p ctx)
+{
+    co->c_state = cco_READY;
+    cco_Context* context = (cco_Context*)ctx;
+    longjmp(*context->restart, context->c_id);
+}
+void cco_return(cco_Coroutine* co, cco_Ctx_p ctx)
+{
+    co->c_state = cco_DONE;
+    cco_Context* context = (cco_Context*)ctx;
+    longjmp(*context->restart, context->c_id);
+}
+
+void cco_send(cco_Coroutine* self, cco_Ctx_p ctx, cco_Co_handle co, char msg[128])
+{
+    cco_Context* context = (cco_Context*)ctx;
+    self->c_state = cco_BLOCKED;
+
+    cco_Message* new_msg = (cco_Message*)calloc(1, sizeof(cco_Message));
+    memcpy(
+        new_msg->data,
+        msg,
+        128);
+    int recipent_id = (long)co;
+    cco_append_msg(context->msg_queue, context->c_id, recipent_id, new_msg);
+    context->procs[recipent_id - 1]->flags |= cco_MESSAGE_PENDING;
+    longjmp(*context->restart, context->c_id);
+}
+cco_Co_handle cco_spawn(cco_Ctx_p ctx, cco_Coroutine spawn)
+{
+    cco_Context* context = (cco_Context*)ctx;
+    return cco_Sched_add_coroutine(context->sched, spawn);
+}
+#endif
+#endif
--- main.c
+++ main.c
@@ -1,112 +1,16 @@
-#include <setjmp.h>
+#define CCO_IMPLEMENTATION
+#include "cco.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef enum CState {
-    READY,
-    BLOCKED,
-    DONE,
-} CState;
-
-typedef struct Coroutine Coroutine;
-
-typedef void* Ctx_p;
-
-typedef void (*poll_fn)(Coroutine* self, Ctx_p ctx);
-
-typedef struct Coroutine {
-    void* state;
-    CState c_state;
-    poll_fn poll;
-} Coroutine;
-
-typedef struct Message {
-    char data[128];
-    int from_id;
-    int to_id;
-    struct Message* prev;
-    struct Message* next;
-} Message;
-
-typedef enum ProcFlags {
-    MESSAGE_PENDING = 1,
-    AWAITING_MESSAGE = 1 << 1,
-} ProcFlags;
-typedef struct Process {
-    Coroutine co;
-    int flags;
-    Message* msg;
-    char* recv_buf;
-    struct Process *next, *prev;
-    int id;
-} Process;
-
-typedef struct MessageQueue {
-    Message* head;
-    Message* tail;
-} MessageQueue;
-
-typedef struct Context {
-    int c_id;
-    jmp_buf* restart;
-    MessageQueue* msg_queue;
-    Process* procs;
-} Context;
-
-void append_msg(MessageQueue* queue, int from, int to, Message* new)
+void* make_spawn1()
 {
-    new->from_id = from;
-    new->to_id = to;
-    if (!queue->head) {
-        queue->head = new;
-        queue->tail = new;
-        queue->head->next = NULL;
-        queue->head->prev = NULL;
-    } else {
-        queue->tail->next = new;
-        new->prev = queue->tail;
-        queue->tail = new;
-        new->next = NULL;
-    }
+    return NULL;
 }
-
-void send(Coroutine* self, Ctx_p ctx, int recipent_id, char msg[128])
+void spawn1(cco_Coroutine* self, cco_Ctx_p ctx)
 {
-    Context* context = (Context*)ctx;
-    self->c_state = BLOCKED;
-
-    Message* new = calloc(1, sizeof(Message));
-    memcpy(
-        new->data,
-        msg,
-        128);
-    append_msg(context->msg_queue, context->c_id, recipent_id, new);
-    context->procs[recipent_id - 1].flags |= MESSAGE_PENDING;
-    longjmp(*context->restart, context->c_id);
-}
-void recv_impl(Coroutine* self, Process* proc, char out[128])
-{
-    proc->recv_buf = out;
-    if ((proc->flags & MESSAGE_PENDING) == MESSAGE_PENDING || proc->msg != NULL) {
-        memcpy(proc->recv_buf,
-            proc->msg->data,
-            128);
-        self->c_state = READY;
-        free(proc->msg);
-        proc->msg = NULL;
-        proc->recv_buf = NULL;
-    } else {
-        proc->flags |= AWAITING_MESSAGE;
-        self->c_state = BLOCKED;
-    }
-}
-void recv(Coroutine* self, Ctx_p ctx, char out[128])
-{
-    Context* context = (Context*)ctx;
-    Process* proc = &context->procs[context->c_id - 1];
-    recv_impl(self, proc, out);
-    longjmp(*context->restart, context->c_id);
+    printf("SPAWN1 => I have been spawned\n");
+    cco_return(self, ctx);
 }
 
 typedef enum SenderStateEnum {
@@ -118,42 +22,47 @@ typedef enum SenderStateEnum {
 typedef struct SenderState {
     SenderStateEnum sstate;
     char* buf;
+    cco_Co_handle* send_to;
 } SenderState;
 
-void sender(Coroutine* self, Ctx_p ctx)
+void sender(cco_Coroutine* self, cco_Ctx_p ctx)
 {
     SenderState* state = (SenderState*)self->state;
     char msg[128] = "Hello, World!";
     switch (state->sstate) {
     case SBeforeSent:
+        printf("SENDER => Spawning spawn1\n");
+        cco_spawn(
+            ctx,
+            cco_make_coroutine(
+                make_spawn1(),
+                spawn1));
         printf("SENDER => Sending message: '%s'\n", msg);
         state->sstate = SAfterSent;
-        send(self, ctx, 2, msg);
+        cco_send(self, ctx, *state->send_to, msg);
         break;
     case SAfterSent:
         printf("SENDER => Awaiting response\n");
         state->sstate = SAfterResponse;
-        recv(self, ctx, state->buf);
+        cco_recv(self, ctx, state->buf);
         break;
     case SAfterResponse:
         printf("SENDER => Got response: '%s'\n", state->buf);
         printf("SENDER => DONE\n");
-        self->c_state = DONE;
+        cco_return(self, ctx);
         break;
     }
 }
 
-Coroutine make_sender()
+void* make_sender(cco_Co_handle* send_to)
 {
     SenderState* state = calloc(1, sizeof(SenderState));
     state->buf = calloc(128, sizeof(char));
     state->sstate = SBeforeSent;
-    return (Coroutine) {
-        .c_state = READY,
-        .state = state,
-        .poll = sender,
-    };
+    state->send_to = send_to;
+    return state;
 }
+
 typedef enum ReceiverStateEnum {
     RBeforeReceive,
     RAfterReceive,
@@ -163,16 +72,17 @@ typedef enum ReceiverStateEnum {
 typedef struct ReceiverState {
     ReceiverStateEnum state_enum;
     char* buf;
+    cco_Co_handle* receive_from;
 } ReceiverState;
 
-void receiver(Coroutine* self, Ctx_p ctx)
+void receiver(cco_Coroutine* self, cco_Ctx_p ctx)
 {
     ReceiverState* state = self->state;
     char resp[128] = "This is a response message";
     switch (state->state_enum) {
     case RBeforeReceive:
         state->state_enum = RAfterReceive;
-        recv(self, ctx, state->buf);
+        cco_recv(self, ctx, state->buf);
         break;
     case RAfterReceive:
         printf("RECEIVER => Received message: '%s'\n",
@@ -180,140 +90,40 @@ void receiver(Coroutine* self, Ctx_p ctx)
         printf("RECEIVER => Responding with: '%s'\n",
             resp);
         state->state_enum = RAfterRespond;
-        send(self, ctx, 1, resp);
+        cco_send(self, ctx, *state->receive_from, resp);
         break;
     case RAfterRespond:
         printf("RECEIVER => DONE\n");
-        self->c_state = DONE;
+        cco_return(self, ctx);
         break;
     }
 }
 
-Coroutine make_receiver()
+void* make_receiver(cco_Co_handle* recieve_from)
 {
     ReceiverState* state = calloc(1, sizeof(ReceiverState));
     state->buf = calloc(128, sizeof(char));
     state->state_enum = RBeforeReceive;
-    return (Coroutine) {
-        .c_state = READY,
-        .state = state,
-        .poll = receiver,
-    };
-}
-
-typedef struct ProcQueue {
-    Process* head;
-    Process* tail;
-} ProcQueue;
-
-void append_proc(ProcQueue* queue, Process* proc)
-{
-    if (!queue->head) {
-        queue->head = proc;
-        queue->tail = proc;
-        queue->head->next = NULL;
-        queue->head->prev = NULL;
-    } else {
-        queue->tail->next = proc;
-        proc->prev = queue->tail;
-        queue->tail = proc;
-        proc->next = NULL;
-    }
-}
-Process* pop_proc(ProcQueue* queue)
-{
-    if (!queue->head) {
-        return NULL;
-    }
-    Process* pop = queue->head;
-    queue->head = queue->head->next;
-    return pop;
-}
-Message* pop_msg(MessageQueue* queue)
-{
-    if (!queue->head) {
-        return NULL;
-    }
-    Message* pop = queue->head;
-    queue->head = queue->head->next;
-    return pop;
-}
-
-void deliver_messages(MessageQueue* mq, Process* procs, int procs_sz)
-{
-    Message* delay_stack[128] = { };
-    int dp = -1;
-    while (mq->head) {
-        Message* msg = pop_msg(mq);
-        Process* to = &procs[msg->to_id - 1];
-        Process* from = &procs[msg->from_id - 1];
-        if (to->msg) {
-            delay_stack[++dp] = msg;
-            continue;
-        } else {
-            to->msg = msg;
-        }
-        to->co.c_state = READY;
-        from->co.c_state = READY;
-        if (to->recv_buf) {
-            recv_impl(&to->co, to, to->recv_buf);
-        }
-    }
-    while(dp >= 0){
-        Message* delayed = delay_stack[--dp];
-        append_msg(mq, delayed->from_id, delayed->to_id, delayed);
-    }
+    state->receive_from = recieve_from;
+    return state;
 }
 
 int main(void)
 {
-    MessageQueue mq = { };
-    Process coroutines[2] = { };
-    coroutines[0] = (Process) {
-        .co = make_sender(),
-        .flags = 0,
-        .msg = NULL,
-        .id = 1,
-    };
-    coroutines[1] = (Process) {
-        .co = make_receiver(),
-        .flags = 0,
-        .msg = NULL,
-        .id = 2,
-    };
-    ProcQueue pqueue = { };
-    jmp_buf jmp_buffer = { };
-    append_proc(&pqueue, &coroutines[0]);
-    append_proc(&pqueue, &coroutines[1]);
-    while (1) {
-        int ret = setjmp(jmp_buffer);
-        if (ret != 0) {
-            append_proc(&pqueue, &coroutines[ret - 1]);
-        }
-        deliver_messages(&mq, coroutines, 2);
-        Process* next = { };
-        next = pop_proc(&pqueue);
-        if (!next) {
-            break;
-        }
-        if (next->co.c_state == BLOCKED) {
-            if ((next->flags & MESSAGE_PENDING) != MESSAGE_PENDING) {
-                append_proc(&pqueue, next);
-                continue;
-            }
-        }
-        if (next->co.c_state == DONE) {
-            continue;
-        }
-        Context ctx = {
-            .procs = coroutines,
-            .c_id = next->id,
-            .msg_queue = &mq,
-            .restart = &jmp_buffer,
-        };
-        next->co.poll(
-            &next->co,
-            &ctx);
-    }
+    cco_Sched sched = cco_Sched_new();
+    cco_Co_handle s = { };
+    cco_Co_handle r = { };
+    s = cco_Sched_add_coroutine(
+        &sched,
+        cco_make_coroutine(
+            make_sender(&r),
+            sender));
+    r = cco_Sched_add_coroutine(
+        &sched,
+        cco_make_coroutine(
+            make_receiver(&s),
+            receiver));
+    cco_Sched_run(&sched);
+    cco_Sched_free(sched);
     return 0;
 }
